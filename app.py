f8bfbb8311c09cce5a91b7179c5c7569eb1a59aa
victory <victorykasende@gmail.com>
2025-05-01 13:00:29 +0100

    Update app.py to configure dynamic port for application deployment

--- app.py
+++ app.py
@@ -49,4 +49,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))  # DO utilise le port 8080 par défaut
+    app.run(host="0.0.0.0", port=port)
